bed63a730e5efde600f2255d857c10b6b7d605d6
Diiego <diego23raposo@gmail.com>
2026-05-09 00:17:15 +0200

    Primera configuración.
    Inicialización de trabajo final / instalación de express / primeras peliculas para la base de datos y servidor base en el puerto 3000.

---
+++ datos.js
@@ -0,0 +1,56 @@
+// Datos previos (no definitivos)
+
+const peliculas = [{
+    id: 1,
+    titulo: "El Caballero Oscuro",
+    genero: "Acción",
+    año: 2008,
+    duracion_minutos: 152,
+    recaudacion_millones: 1004.9,
+    nota_imdb: 9.0,
+    ganadora_oscar: true
+    },
+    {
+        id: 2,
+        titulo: "Pulp Fiction",
+        director: "Quentin Tarantino",
+        genero: "Crimen",
+        año: 1994,
+        duracion_minutos: 154,
+        recaudacion_millones: 213.9,
+        nota_imdb: 8.9,
+        ganadora_oscar: true
+    },
+    {
+        id: 3,
+        titulo: "Dune: Parte Dos",
+        director: "Denis Villeneuve",
+        genero: "Ciencia Ficción",
+        año: 2024,
+        duracion_minutos: 166,
+        recaudacion_millones: 711.8,
+        nota_imdb: 8.6,
+        ganadora_oscar: false
+    }
+];
+
+const criticas = [
+    {
+        id_critica: 101,
+        pelicula_id: 1, // Vinculado a "El Caballero Oscuro"
+        usuario: "Cinefilo1",
+        comentario: "Grande el joker.",
+        estrellas: 5
+    },
+    {
+        id_critica: 102,
+        pelicula_id: 2, // Vinculado a "Pulp Fiction"
+        usuario: "Cinefilo2",
+        comentario: "Buenos díalogos.",
+        estrellas: 5
+    }
+];
+
+// Exportación con CommonJS.
+module.exports = { peliculas, criticas };
+
--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log("Hola")
-console.log("Hago una API")
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// importación de datos simulados
+const { peliculas, criticas } = require('./datos.js');
+
+// Middleware para que Express entienda que es un formato JSON en las peticiones..
+app.use(express.json());
+
+// Endpoint de prueba
+app.get('/', (req, res) => {
+    res.send('API de Cine de Diego')
+});
+
+// Mensaje cuando arranca el servidor.
+app.listen(PORT, () => {
+    console.log('Servidor de todas las películas que he visto en 2025 en http://localhost:${PORT}');
+})
